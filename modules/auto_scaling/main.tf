e419daee36cc3a3ce7c286448ec64bcbeb3afcc5
Evan Glicker <eaglicker@gmail.com>
2025-07-07 02:06:00 -0400

    update auto scaling module

--- modules/auto_scaling/main.tf
+++ modules/auto_scaling/main.tf
@@ -12,14 +12,21 @@ provider "aws" {
 }
 
 resource "aws_launch_template" "main" {
-  name_prefix   = var.launch_template.prefix
-  image_id      = var.launch_template.ami
-  instance_type = var.launch_template.instance_class
-  tags = var.tags
+  name_prefix   =          var.launch_template.prefix
+  image_id               = var.launch_template.ami
+  instance_type          = var.launch_template.instance_class
+  description            = var.launch_template.description
+  tags                   = var.tags
+  key_name               = var.launch_template.key_name
+  user_data              = var.launch_template.user_data
+  vpc_security_group_ids = [var.launch_template.security_group_id]
+  monitoring {
+    enabled = var.launch_template.detailed_monitoring
+  }
 }
 
 resource "aws_autoscaling_group" "main" {
-  availability_zones = var.scaling_group.availability_zones
+  vpc_zone_identifier = var.scaling_group.subnet_ids
   desired_capacity   = var.scaling_group.desired_capacity
   max_size           = var.scaling_group.max_size
   min_size           = var.scaling_group.min_size
@@ -35,4 +42,38 @@ resource "aws_autoscaling_group" "main" {
       propagate_at_launch = true
     }
   }
+  target_group_arns = var.attach_to_lb ? [var.lb_arn] : [""]
+}
+
+resource "aws_autoscaling_policy" "policies" {
+    for_each = var.scaling_policies
+    name                   = each.value.name
+    scaling_adjustment     = each.value.scaling_adjustment
+    adjustment_type        = each.value.adjustment_type
+    cooldown               = each.value.cooldown
+    autoscaling_group_name = aws_autoscaling_group.main.name
+    policy_type = "SimpleScaling"
+}
+
+# Attach the Auto Scaling Group to the ALB target group
+resource "aws_autoscaling_attachment" "my_asg_attachment" {
+  count = var.attach_to_lb ? 1 : 0
+    autoscaling_group_name = aws_autoscaling_group.main.name
+    lb_target_group_arn = var.lb_arn
+}
+
+resource "aws_cloudwatch_metric_alarm" "reduce_ec2_alarm" {
+  for_each = var.cloudwatch_alarms
+  alarm_name                = each.value.name
+  comparison_operator       = each.value.comparison_operator
+  evaluation_periods        = each.value.evaluation_periods
+  metric_name               = each.value.metric_name
+  namespace                 = "AWS/EC2"
+  period                    = each.value.period
+  statistic                 = each.value.statistic
+  threshold                 = each.value.threshold
+  alarm_description         = each.value.alarm_description
+  insufficient_data_actions = []
+
+  alarm_actions = ["${aws_autoscaling_policy.policies[each.value.policy_to_use].arn}"]
 }
--- modules/auto_scaling/vars.tf
+++ modules/auto_scaling/vars.tf
@@ -8,12 +8,17 @@ variable "launch_template" {
     ami            = string
     prefix         = string
     instance_class = string
+    description    = optional(string)
+    detailed_monitoring = optional(bool, false)
+    key_name = optional(string)
+    security_group_id = optional(string)
+    user_data = optional(string)
   })
 }
 
 variable "scaling_group" {
   type=object({
-    availability_zones = list(string)
+    subnet_ids         = optional(list(string))
     desired_capacity   = number
     max_size           = number
     min_size           = number
@@ -23,4 +28,37 @@ variable "scaling_group" {
 variable "tags" {
   type = map(string)
   default = {}
+}
+
+variable "scaling_policies" {
+  type=map(object({
+    name               = string
+    scaling_adjustment = number
+    adjustment_type    = string
+    cooldown           = number
+  }))
+}
+
+variable "attach_to_lb" {
+  type = bool
+  default = false
+}
+
+variable "lb_arn" {
+  type = string
+  default = ""
+}
+variable "cloudwatch_alarms" {
+  type = map(object({
+    name = string
+    comparison_operator = string
+    evaluation_periods = number
+    metric_name = string
+    period = number
+    statistic = string
+    threshold = number
+    alarm_description = string
+    insufficient_data_actions = optional(list(string))
+    policy_to_use = string
+  }))
 }
