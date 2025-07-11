03f32196b2a12578d678d55db53744011ac32b7d
Gafur Norbaev <gafurnorbaev@Gafurs-MacBook-Pro.local>
2025-07-11 16:38:14 -0500

    my_first_commit

---
+++ modules/rds_module/vars.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  description = "Name prefix for RDS resources"
+  type        = string
+  #value = "Gafur_test"
+}
+ 
+variable "engine" {
+  default     = "mysql"
+  type        = string
+}
+ 
+variable "engine_version" {
+  default     = "8.0"
+  type        = string
+}
+ 
+variable "instance_class" {
+  description = "DB instance type (e.g., db.t3.micro)"
+  type        = string
+}
+ 
+variable "allocated_storage" {
+  default = 20
+  type    = number
+}
+ 
+variable "username" {
+  type        = string
+}
+ 
+variable "password" {
+  type        = string
+  sensitive   = true
+}
+ 
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
